3ea776e23b9539b179d9dbe6c40edd8f34430fce
Javohir <ismoilov92@gmail.com>
2023-07-01 20:34:47 +0300

    Revert "fix: add password field to user table"
    This reverts commit 214d5d0ea57ae8586cfc7623bae7d366a2a365f6.

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,8 +1,9 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE Users
 (
     id               INT AUTO_INCREMENT PRIMARY KEY,
-    username         VARCHAR(255) UNIQUE NOT NULL,
-    password         VARCHAR(255)        NOT NULL,
+    username         VARCHAR(255) NOT NULL,
     telegram_chat_id INT
 );
 
--- src/main/resources/db/migration/V2__create_subscriptions_table.sql
+++ src/main/resources/db/migration/V2__create_subscriptions_table.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Subscriptions
-(
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    query   VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES Users (id)
+CREATE TABLE Subscriptions (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               user_id INT,
+                               query VARCHAR(255),
+                               FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE INDEX idx_query ON Subscriptions (query);
