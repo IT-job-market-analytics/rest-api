4af7b5617d5c61c9a4f7c1ab92af3b05df729979
Javohir <ismoilov92@gmail.com>
2023-07-17 04:23:21 +0300

    fix: add queries to config file

--- src/main/java/com/example/restapi/services/SubscriptionsService.java
+++ src/main/java/com/example/restapi/services/SubscriptionsService.java
@@ -1,6 +1,5 @@
 package com.example.restapi.services;
 
-import com.example.restapi.config.AllAvailableQueries;
 import com.example.restapi.dto.subscription.SubscriptionDto;
 import com.example.restapi.exceptions.QueryNotFoundExceptions;
 import com.example.restapi.exceptions.ResourceNotFoundException;
@@ -11,10 +10,12 @@ import com.example.restapi.models.User;
 import com.example.restapi.repositories.SubscriptionRepository;
 import com.example.restapi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -23,7 +24,9 @@ public class SubscriptionsService {
     private final UserRepository userRepository;
     private final SubscriptionRepository subscriptionRepository;
     private final SubscriptionsMapper subscriptionsMapper;
-    private final AllAvailableQueries allAvailableQueries;
+
+    @Value("${available.queries}")
+    private String queries;
 
     public List<SubscriptionDto> getSubscriptions(String username) {
         return subscriptionsMapper.toDtoList(
@@ -36,7 +39,7 @@ public class SubscriptionsService {
     }
 
     public List<SubscriptionDto> getAllAvailableSubscriptions() {
-        return allAvailableQueries.allAvailable();
+        return allAvailable();
     }
 
     @Transactional
@@ -51,7 +54,7 @@ public class SubscriptionsService {
                             .query(query)
                             .build()
             );
-        }catch (DbActionExecutionException e){
+        } catch (DbActionExecutionException e) {
             throw new ValueAlreadyExistsException("User already subscribed to this query");
         }
         return getSubscriptions(username);
@@ -75,12 +78,19 @@ public class SubscriptionsService {
     }
 
     private void checkAllAvailableQueries(String query) {
-        allAvailableQueries.allAvailable().stream()
+        allAvailable().stream()
                 .map(SubscriptionDto::getQuery)
                 .filter(q -> q.equalsIgnoreCase(query))
                 .findFirst()
                 .orElseThrow(() -> new QueryNotFoundExceptions("Non supported query"));
     }
 
+    private List<SubscriptionDto> allAvailable() {
+        String[] queryArray = queries.split(" ");
 
+        return Arrays.stream(queryArray)
+                .map(String::trim)
+                .map(SubscriptionDto::new)
+                .toList();
+    }
 }
