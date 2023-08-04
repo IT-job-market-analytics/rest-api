3fd28544c2cf7841331822ba844c481760597d02
Sergey Zhukov <zhukovsd@gmail.com>
2023-08-04 20:29:29 +0400

    chore: test endpoint moved under `/test` path

--- src/main/java/com/example/restapi/controllers/VacancyAnalyticsDataPointController.java
+++ src/main/java/com/example/restapi/controllers/VacancyAnalyticsDataPointController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/vacancy")
+@RequestMapping("/test/vacancy")
 @Slf4j
 public class VacancyAnalyticsDataPointController {
     private final VacancyAnalyticsDataPointService vacancyAnalyticsDataPointService;
